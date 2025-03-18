1bd30c9511055754a92905d57299eef56ba20796
starcrafter1345 <starcrafter1345@gmail.com>
2025-03-18 13:32:34 +0100

    task completed

---
+++ index.js
@@ -0,0 +1,25 @@
+export default class ECTS {
+  constructor(score) {
+    this.score = Number(score);
+  }
+
+  ectsFromScore() {
+    let ectsScore;
+
+    if (90 <= this.score && this.score <= 100) {
+      ectsScore = "A";
+    } else if (82 <= this.score && this.score <= 89) {
+      ectsScore = "B";
+    } else if (74 <= this.score && this.score <= 81) {
+      ectsScore = "C";
+    } else if (65 <= this.score && this.score <= 73) {
+      ectsScore = "D";
+    } else if (60 <= this.score && this.score <= 64) {
+      ectsScore = "E"
+    } else {
+      ectsScore = "F"
+    }
+
+    return ectsScore;
+  }
+}
